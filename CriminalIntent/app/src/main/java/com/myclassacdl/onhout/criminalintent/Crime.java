443aaa5ea5eb599fa8157c95911da96c469610b4
Peter Liu <onhout@gmail.com>
2014-08-13 21:11:06 -0700

    Second app initial commit

---
+++ CriminalIntent/app/src/main/java/com/myclassacdl/onhout/criminalintent/Crime.java
@@ -0,0 +1,25 @@
+package com.myclassacdl.onhout.criminalintent;
+
+import java.util.UUID;
+
+
+public class Crime {
+    private UUID id;
+    private String title;
+
+    public Crime(){
+        id = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
