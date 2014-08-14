42298d6bc10bdc17e67ca6d614d0a77f1176f8cb
Peter Liu <onhout@gmail.com>
2014-08-13 23:36:22 -0700

    Added first fragment

---
+++ CriminalIntent/app/src/main/java/com/myclassacdl/onhout/criminalintent/CrimeFragment.java
@@ -0,0 +1,45 @@
+package com.myclassacdl.onhout.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+    private EditText titleField;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+        titleField = (EditText)v.findViewById(R.id.crime_title);
+        titleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        return v;
+    }
+}
